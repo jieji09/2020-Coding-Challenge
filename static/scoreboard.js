6d3a061285e46f69b67336d80080375ff3b5fe2a
Jie <jj3291@barnard.edu>
2025-11-22 15:32:53 -0500

    Implement real-time score updates and automatic sorting

--- static/scoreboard.js
+++ static/scoreboard.js
@@ -6,14 +6,16 @@ function display_scoreboard(scoreboard){
 }
 
 function addTeamView(id, name, score){
-  var team_template = $("<div class = row></div>");
-  var name_template = $("<div class = col-md-5></div>");
-  var score_template = $("<div class = col-md-2></div>");
-  var button_template = $("<div class = col-md-2></div>");
-  var increase_button = $("<button class = increase-button>+</button>");
+  var team_template = $("<div class='team-row'></div>");
+  var name_template = $("<div class='team-name'></div>");
+  var score_template = $("<div class='team-score'></div>");
+  var button_template = $("<div class='button-container'></div>");
+  var increase_button = $("<button class='increase-button'>+</button>");
+  
   $(increase_button).click(function(){
     increase_score(id);
   });
+  
   name_template.text(name);
   score_template.text(score);
   button_template.append(increase_button);
@@ -32,7 +34,7 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+        display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
